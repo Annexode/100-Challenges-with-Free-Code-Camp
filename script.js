3451ab9775dfab92b2caedd941548045559593e6
Annexode <annexode@gmail.com>
2024-11-02 06:07:22 +0200

    challengeOne Comleted

---
+++ script.js
@@ -0,0 +1,10 @@
+let countingDisplay = document.getElementById("counting");
+
+
+
+let count = 0;
+function counts() {
+  count = count + 1;
+  countingDisplay.innerText = count;
+  console.log(count);
+}
