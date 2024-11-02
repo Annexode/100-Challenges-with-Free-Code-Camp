3b27ddb1b4aa40991e73ed9c12fdc5253686e268
Annexode <annexode@gmail.com>
2024-11-02 07:14:55 +0200

    save button created

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ let countingDisplay = document.getElementById("counting");
 
 
 
+
 let count = 0;
 function counts() {
   count = count + 1;
   countingDisplay.innerText = count;
   console.log(count);
+}
+
+function save() {
+  console.log(count);
+  
 }
