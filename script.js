9610783e60beb1341c9fb697a1992aa9599158ad
Annexode <annexode@gmail.com>
2024-11-04 19:54:49 +0200

    chellengeOne completed

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 let countingDisplay = document.getElementById("counting");
-
-
-
+let previousCounting = document.getElementById("previousEntries");
 
 let count = 0;
 function counts() {
-  count = count + 1;
+  //count = count + 1; OR
+  count += 1;
   countingDisplay.innerText = count;
   console.log(count);
 }
 
 function save() {
+  let previuosEntriesString = count + " - ";
+  previousCounting.textContent += previuosEntriesString;
+  countingDisplay.innerText = 0;
+  count = 0;
   console.log(count);
-  
 }
