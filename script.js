a8892cdc76b72c091f7c4138167b5307c0240b8a
Annexode <annexode@gmail.com>
2024-11-11 08:21:19 +0200

    complete challenge two, create simple calculator of all operations.

--- script.js
+++ script.js
@@ -16,3 +16,72 @@ function save() {
   count = 0;
   console.log(count);
 }
+
+// RECAP
+firstName = "Annexode";
+lastName = "IGIRANEZA";
+
+fullNames = firstName + " " + lastName;
+console.log(fullNames);
+
+let myPoints = 3;
+function add3Points() {
+  myPoints = myPoints + 3;
+}
+
+function remove1Point() {
+  myPoints = myPoints - 1;
+}
+
+add3Points();
+add3Points();
+add3Points();
+remove1Point();
+remove1Point();
+console.log(myPoints);
+
+//NIKE SHOES
+
+let message = document.getElementById("error");
+function errorMessage() {
+  console.log("clicked");
+  message.innerHTML = "Try again";
+}
+
+//CALCULATOR
+
+let num1 = 8;
+let num2 = 2;
+let result = document.getElementById("result");
+
+let num1_el = document.getElementById("num1_el");
+let num2_el = document.getElementById("num2_el");
+
+num1_el.innerHTML = num1;
+num2_el.innerHTML = num2;
+
+function add() {
+  let addition = num1 + num2;
+  result.innerHTML = "SUM:" +" " + addition;
+  console.log(addition);
+}
+
+function substract() {
+  let substraction = num1 - num2;
+  result.innerHTML = "SUM:" +" " + substraction;
+  console.log(substraction);
+}
+
+function divide() {
+  let division = num1 / num2;
+  result.innerHTML = "SUM:" +" " + division;
+  console.log(division);
+}
+
+function multiply() {
+  let multiplication = num1 * num2;
+  result.innerHTML = "SUM:" +" " + multiplication;
+  console.log(multiplication);
+}
+
+
